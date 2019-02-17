50df2f1cc67f0c8080bc674162f73d8d39086838
Krukovich <44588100+Krukovich@users.noreply.github.com>
2019-02-17 21:56:15 +0300

    add js_hw_7 and js_hw_8

---
+++ js_hw_7/js/script.js
@@ -0,0 +1,85 @@
+'use strict'
+
+// Задача 1. Напишите функцию, которая принимает в качестве параметра номер символа в таблице Unicode.
+// Подсчитайте количество таких символов во всех элементах массива, как в задаче 3 из первой части практики
+
+// let str = "Напишите функцию, которая принимает в качестве параметра номер символа в таблице Unicode";
+// let arrayStr = str.split("");
+//
+// function sumCharUnicodeInStr(massive, uni) {
+//   let result = massive.filter(function(item) {
+//     return item.charCodeAt() == uni;
+//
+//   });
+//   console.log(result.length);
+// }
+// sumCharUnicodeInStr(arrayStr,"1072"); // код маленькой буквы а
+
+// Задача 2. Напишите функцию, которая будет возвращать частичную функцию от функции из задачи 1, фиксируя искомый символ. Например:
+//
+// let letterCounter = sumCharUnicodeInStr(arrayStr,1072);
+// console.log(letterCounter);
+
+// Для решения задач 3-6 используйте файл uscities.js.
+// массив data берется из файла uscities.js
+
+// Задача 3. Отфильтруйте массив городов так, чтобы в нём остались только города из штата Калифорния, которые с 2000 по 2013 выросли в населении.
+
+function citySort() {
+  let citySortArray = data.filter(function(item) { // массив data берется из файла uscities.js
+     return item.state == "California" && parseInt(item.growth_from_2000_to_2013) > 0;
+  });
+  return citySortArray;
+}
+citySort();
+
+// Задача 4. Подсчитайте, сколько миллионов населения живёт во всех городах на широте от 25 до 30 градусов.
+//
+// function countPeoples() {
+//   let peopleCount = 0;
+//   let array = data.filter(function(item) {
+//     return parseInt(item.latitude) > 25 && parseInt(item.latitude) < 30;
+//   });
+//   array.forEach(function(item) {
+//     return peopleCount += parseInt(item.population);
+//   });
+//   return peopleCount;
+// }
+// countPeoples();
+
+// Задача 5. Создайте массив только из тех городов, которые начинаются на букву D, при этом отсортируйте элементы этого массива по названию города.
+//
+// function arrayFirstCharD() {
+//   let array = data.filter(function(item) {
+//     return item.city.startsWith("D");
+//   });
+//   array.sort(function(a, b) {
+//     if (a.city > b.city) {
+//       return 1;
+//     } else {
+//       return -1;
+//     }
+//   });
+//   return array;
+// }
+// arrayFirstCharD();
+
+// Задача 6. Преобразуйте представленный массив "Города" в объект "Штаты" :для каждого штата – отдельное свойство объекта (ключ = название штата);
+// значение каждого свойства – массив городов; каждый элемент массива – название города, население и место в общем рейтинге (rank).
+//
+// function createObjectState() {
+//   let states = {};
+//   for (var i = 0; i < data.length; i++) {
+//     states[data[i]["state"]] = [];
+//     states[data[i]["state"]].push(data[i]["city"]);
+//   }
+//   for (var key in states) {
+//     for (var j = 0; j < data.length; j++) {
+//       if (key == data[j]["state"]) {
+//         states[data[j]["state"]].push(data[j]["city"]);
+//       }
+//     }
+//   }
+//   console.log(states);
+// }
+// createObjectState();
---
+++ js_hw_8/js/script.js
@@ -0,0 +1,105 @@
+'use strict'
+
+// Задача 1. Дан элемент #elem. Добавьте ему класс "www"
+
+let newDiv = document.createElement('div');
+newDiv.className = "www";
+document.body.appendChild(newDiv);
+
+// Задача 2. Дан элемент #elem. Проверьте наличие у него класса "www", если есть - удалите класс
+
+function removeClassName(doc, tag) {
+  let foundTags = doc.getElementsByTagName(tag);
+  for (var i = 0; i < foundTags.length; i++) {
+    if (foundTags[i].className == "www") {
+      foundTags[i].className = "";
+    }
+  }
+}
+removeClassName(document.body, "div")
+
+// Задача 3. Напишите функцию поиска recursiveSearchTags, которая принимает на вход document и имя тега,
+// а возвращает массив из всех элементов соответствующих этому тегу. Поиск идет в теге body
+
+function recursiveSearchTags(doc, tag) {
+  let result = doc.getElementsByTagName(tag);
+  result = [].slice.call(result);
+  return result
+};
+recursiveSearchTags(document.body, 'section');
+
+// Задача 4. Дан ul. Дан массив. Вставьте элементы этого массива в конец ul так, чтобы каждый элемент стоял в своем li.
+// Сделайте так, чтобы четные позиции имели красный фон.
+
+let array = ["Петя", "Саша", "Артём", "Сергей", "Алексей", "Кирилл", "Олег"];
+
+function addArrayItemInUl(arr) {
+  let parent = document.body;
+  let ul = document.createElement("ul");
+  for (var i = 0; i < arr.length; i++) {
+    let li = document.createElement("li");
+    let text = document.createTextNode(arr[i]);
+    if (i % 2) {
+      li.setAttribute("style", "background-color: red; width: 100px");
+    }
+    li.appendChild(text);
+    ul.appendChild(li);
+    parent.appendChild(ul);
+  }
+}
+addArrayItemInUl(array);
+
+// Задание 5. Дан элемент #elem. Найдите его соседа сверху и добавьте ему в конец текст '!'
+
+let searchDiv = document.querySelector(".task-five");
+let nextDivUp = searchDiv.previousElementSibling;
+let contentDivUp = nextDivUp.innerHTML;
+let text = document.createTextNode("!!!!!!!");
+let inserText = nextDivUp.appendChild(text);
+document.body.insertBefore(nextDivUp, searchDiv);
+
+// Задача 6. Реализуйте функцию wrapInParagraph, которая находит текст (дочерние текстовые ноды) внутри элемента div и оборачивает текст в параграф
+
+function wrapInParagraph(doc, tegClass) {
+  let p = document.createElement("p");
+  let searchClass = document.querySelector(tegClass);
+  let nextTeg = searchClass.lastElementChild;
+  let content = nextTeg.innerHTML;
+  let arr = [];
+  arr.push(content);
+  for (var i = 0; i < arr.length; i++) {
+    let text = document.createTextNode(arr[i]);
+    p.appendChild(text);
+    nextTeg.appendChild(p);
+    searchClass.appendChild(nextTeg);
+    doc.appendChild(searchClass);
+  }
+}
+wrapInParagraph(document.body, ".task-six");
+
+// Задача 7. Реализуйте функцию normalizeClassNames, которая нормализует имена классов для всех элементов на странице.
+// В данном случае это означает что происходит преобразование всех классов написанных используя kebab нотацию в camelCase нотацию: text-center => textCenter
+
+function normalizeClassNames() {
+  let searchClass = document.body.getElementsByTagName("*");
+  for (var i = 0; i < searchClass.length; i++) {
+    let item = searchClass[i];
+    if (item.classList.length > 0) {
+      for (var j = 0; j < item.classList.length; j++) {
+        let tempClass = item.classList[j];
+        let arrClassName = tempClass.split("");
+        for (var k = 0; k < arrClassName.length; k++) {
+          if (arrClassName[k] == "-") {
+            delete arrClassName[k];
+            let tempStr = arrClassName[k + 1];
+            let tempSrtUpper = String(tempStr.toUpperCase());
+            arrClassName[k + 1] = tempSrtUpper;
+          }
+        }
+        let strClass = arrClassName.join("");
+        item.className = strClass;
+      }
+    }
+  }
+}
+normalizeClassNames();
